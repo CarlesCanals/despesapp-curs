bf698705aa715d1537d6c9108ba564e3eb816904
Carles Canals <cgc@paucasesnovescifp.cat>
2025-04-09 11:58:27 +0200

    Actualiza el componente App para mostrar un título, subtítulo y datos dinámicos, eliminando contenido innecesario.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const titol = 'Benvinguts al curs!';
+  const subtitol = 'React i Firebase';
+
+  const emailLink = 'mailto:info@despesapp.com';
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <header>
+      <h1>{titol}</h1>
+      <h3>{subtitol}</h3>
+      <p>La data d'avui és: {new Date().toLocaleDateString('ca-ES', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })}</p>
+      <p>Nombre aleatori: {Math.floor(Math.random() * 101)}</p>
+      <a href={emailLink}>Contacta'ns</a>
+      </header>
+    </div>
   )
 }
 
