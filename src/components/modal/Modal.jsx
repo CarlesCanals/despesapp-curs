e5f54b0c8058d86d8c8de3d965fbaca6e938ac25
Carles Canals <cgc@paucasesnovescifp.cat>
2025-04-30 10:19:14 +0200

    Añade el componente Modal y actualiza App para gestionar su visualización; modifica Titol para usar fragmentos.

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,12 @@
+import './Modal.css';
+
+export default function Modal({ children, handleTancar }) {
+    return (
+        <div className="modal-fons">
+            <div className="modal">
+                {children}
+                <button onClick={handleTancar}>Tancar</button>
+            </div>
+        </div>
+    )
+}
--- src/components/titol/Titol.jsx
+++ src/components/titol/Titol.jsx
@@ -2,9 +2,9 @@ import './Titol.css';
 
 export default function Titol(props) {
     return (    
-        <div>
+        <>
             <h1>{props.titol}</h1>
             <h2>{props.subtitol}</h2>
-        </div>
+        </>
     )
 }
